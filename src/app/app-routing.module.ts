8b451cbede99d4f28dbc9d87465596c9a37c9c3a
rivka.eizen <rivka.eizen@riscogroup.com>
2018-06-26 12:42:03 +0300

    rivkae start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {
     Routes,
     RouterModule
 } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
+import { LoginComponent } from './login-module/login.component';
 
 
 export const routes: Routes = [
